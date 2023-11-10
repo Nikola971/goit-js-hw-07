e1df8778363c56a362d5f05b752b067b3ff23c03
Nikola971 <mukola.milyan@outlook.com>
2023-11-10 18:52:05 +0200

    іваіва

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+
+function renderGallery() {
+  const galleryMarkup = galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>
+      `;
+    })
+    .join('');
+
+  gallery.innerHTML = galleryMarkup;
+}
+
+renderGallery();
+
+function openModal(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const largeImageUrl = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageUrl}" alt="Image description">
+  `);
+
+  instance.show();
+
+  document.addEventListener('keydown', closeModalOnEscape);
+
+  function closeModalOnEscape(e) {
+    if (e.code === 'Escape') {
+      instance.close();
+      document.removeEventListener('keydown', closeModalOnEscape);
+    }
+  }
+}
+gallery.addEventListener('click', openModal);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
+// 02-lightbox.js
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Select the gallery container
+const galleryContainer = document.querySelector('.gallery');
+
+// Function to create gallery items
+function createGalleryItem({ preview, original, description }) {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const link = document.createElement('a');
+  link.classList.add('gallery__link');
+  link.setAttribute('href', original);
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.setAttribute('src', preview);
+  image.setAttribute('alt', description);
+
+  link.appendChild(image);
+  galleryItem.appendChild(link);
+
+  return galleryItem;
+}
+
+// Render gallery items
+galleryItems.forEach(item => {
+  const galleryItem = createGalleryItem(item);
+  galleryContainer.appendChild(galleryItem);
+});
+
+// Initialize SimpleLightbox
+document.addEventListener('DOMContentLoaded', function () {
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+    close: false, // Додаткова опція, щоб уникнути закриття лайтбоксу
+  });
+});
